c62cdc5c3260f342889113beea0dc3e5ad9dacb7
CodeGallantX <johnayobami77@proton.me>
2024-06-15 00:51:53 +0100

    "Updated HTML, CSS, and JavaScript files: added new styles and functionality to the navigation menu, hero section, destinations, and tours sections; implemented a sidebar with toggle functionality; and made various layout and design changes."

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
-// Smooth Scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
-
-// Image Slider
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.getElementsByClassName("slideshow-slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change slide every 5 seconds
+function showSideBar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.right = '0px';
 }
 
-// Dynamic Navigation Menu
-window.addEventListener("scroll", function () {
-  let header = document.querySelector("header");
-  let navLinks = document.querySelectorAll(".menu li a");
-  
-  if (window.pageYOffset > 100) {
-    header.classList.add("scroll");
-    navLinks.forEach(link => {
-      link.classList.add("scroll");
-    });
+function hideSideBar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.right = '-100rem';
+}
+
+
+document.querySelector('.see-more').addEventListener('click', function() {
+  var moreTours = document.querySelector('.more-tours');
+  if (moreTours.style.display === 'none' || moreTours.style.display === '') {
+      moreTours.style.display = 'block';
+      this.textContent = 'Show Less';
   } else {
-    header.classList.remove("scroll");
-    navLinks.forEach(link => {
-      link.classList.remove("scroll");
-    });
+      moreTours.style.display = 'none';
+      this.textContent = 'See More';
   }
 });
